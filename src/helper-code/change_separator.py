725afb7e8ad4b7fb7f384e2a6f7eb6dc137b78e6
dimitrismit <46426319+dimitrismit@users.noreply.github.com>
2024-01-24 04:40:28 +0200

    Update change_separator.py

--- src/helper-code/change_separator.py
+++ src/helper-code/change_separator.py
@@ -1,10 +1,26 @@
 import csv
 import sys
+import os
 
-file = sys.argv[1]
+input_dir = '/Users/dimitris.mitr/Desktop/Σχολή/11ο εξάμηνο/advanced DB/updated_results/'
+output_dir = '/Users/dimitris.mitr/Desktop/Σχολή/11ο εξάμηνο/advanced DB/updated_results_comma/'
 
-reader = csv.reader(open(file, "rU"), delimiter='|')
-writer = csv.writer(open(file, 'w'), delimiter=',')
-writer.writerows(reader)
+folders_list = [
+	'query1',
+	'query2',
+	'query3',
+	'query4'
+]
 
-print("Delimiter successfully changed")
+
+for folder in folders_list:
+	for subdir, dirs, files in os.walk(input_dir+folder):
+		for file in files:
+			if file.endswith('.csv'):
+				input_file_path = os.path.join(subdir, file)
+				output_file_path = os.path.join(subdir.replace('/updated_results', '/updated_results_comma'), file)
+				#print("output_file_path is:" ,output_file_path)
+				reader = csv.reader(open(input_file_path, newline = None), delimiter='|')
+				writer = csv.writer(open(output_file_path,  'w', encoding="utf-16"), delimiter=',')
+				writer.writerows(reader)
+				print("Delimiter successfully changed")
