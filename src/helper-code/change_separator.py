b4f305590f2b0ac34a4c523a32faf5993cf4e4e5
dimitrismit <46426319+dimitrismit@users.noreply.github.com>
2024-01-24 16:19:46 +0200

    Update change_separator.py

--- src/helper-code/change_separator.py
+++ src/helper-code/change_separator.py
@@ -2,8 +2,23 @@ import csv
 import sys
 import os
 
+#It is assumed that the structure will look something like this
+#├── new_folder
+#│   ├── hint_and_eplain
+#│   ├── query1
+#│   ├── query2
+#│   ├── query3
+#│   └── query4
+#└── old_folder
+#    ├── hint_and_eplain
+#    ├── query1
+#    ├── query2
+#    ├── query3
+#    └── query4
 results_dir = '/path/to/original/results/folder'
 results_comma_directory = '/path/to/updated/results/folder'
+old_folder = '/old_folder_name'
+new_folder = '/new_folder_name'
 
 folders_list = [
 	'query1',
@@ -18,7 +33,7 @@ for folder in folders_list:
 		for file in files:
 			if file.endswith('.csv'):
 				input_file_path = os.path.join(subdir, file)
-				output_file_path = os.path.join(subdir.replace('/updated_results', '/updated_results_comma'), file)
+				output_file_path = os.path.join(subdir.replace(old_folder, new_folder), file)
 				#print("output_file_path is:" ,output_file_path)
 				reader = csv.reader(open(input_file_path, newline = None), delimiter='|')
 				writer = csv.writer(open(output_file_path,  'w'), delimiter=',')
