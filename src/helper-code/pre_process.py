7dece2ea5a9c8ead1a6598711a4e12f8cc4768d5
dimitrismit <46426319+dimitrismit@users.noreply.github.com>
2024-01-25 21:42:02 +0200

    Update pre_process.py

--- src/helper-code/pre_process.py
+++ src/helper-code/pre_process.py
@@ -1,9 +1,8 @@
 from pyspark.sql import SparkSession
 #from pyspark.conf import SparkConf
-from pyspark.sql.types import DateType, IntegerType, DoubleType
-from pyspark.sql.functions import col, to_date
+from pyspark.sql.types import DateType, IntegerType, DoubleType, StructType, StructField
+from pyspark.sql.functions import col, to_date, lit, create_map
 from lib import store_file
-from pyspark.sql.functions import lit, create_map
 from itertools import chain
 import sys
 
@@ -17,6 +16,7 @@ print('-'*100)
 print("Spark session started!")
 print('-'*100)
 spark.sparkContext.setLogLevel("WARN")
+spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 #config = spark.sparkContext.getConf().getAll()
 #conf = SparkConf()
 #print(config)
@@ -54,33 +54,41 @@ schemaString = df_2010_to_2019._jdf.schema().treeString()
 print(schemaString)
 print("-"*100)
 
+df_2020_to_present = df_2020_to_present.withColumnRenamed('Area', 'Area ')
+df = df_2010_to_2019.unionByName(df_2020_to_present)
+
 #change column types
 
-df_2010_to_2019 = df_2010_to_2019.withColumn("Date Rptd", to_date(df_2010_to_2019["Date Rptd"], "MM-dd-yyyy").cast(DateType()))
-df_2010_to_2019 = df_2010_to_2019.withColumn("DATE OCC", to_date(df_2010_to_2019["DATE OCC"], "MM-dd-yyyy").cast(DateType()))
-#df.withColumn("DATE OCC",col("DATE OCC").cast(DateType()))
-df_2010_to_2019 = df_2010_to_2019.withColumn("Vict Age",col("Vict Age").cast(IntegerType()))
-df_2010_to_2019 = df_2010_to_2019.withColumn("LAT",col("LAT").cast(DoubleType()))
-df_2010_to_2019 = df_2010_to_2019.withColumn("LON",col("LON").cast(DoubleType()))
-print("This is the updated schema")
-schemaString = df_2010_to_2019._jdf.schema().treeString()
-print(schemaString)
-print("-"*100)
+new_schema = StructType([
+    StructField("Vict Age", IntegerType(), True),
+    StructField("LAT", DoubleType(), True),
+    StructField("LON", DoubleType(), True),
+    StructField("Date Rptd", DateType(), True),
+    StructField("DATE OCC", DateType(), True)
+])
 
-df_2020_to_present = df_2020_to_present.withColumn("Date Rptd", to_date(df_2020_to_present["Date Rptd"], "MM-dd-yyyy").cast(DateType()))
-df_2020_to_present =df_2020_to_present.withColumn("DATE OCC", to_date(df_2020_to_present["DATE OCC"], "MM-dd-yyyy").cast(DateType()))
-#df.withColumn("DATE OCC",col("DATE OCC").cast(DateType()))
-df_2020_to_present =df_2020_to_present.withColumn("Vict Age",col("Vict Age").cast(IntegerType()))
-df_2020_to_present =df_2020_to_present.withColumn("LAT",col("LAT").cast(DoubleType()))
-df_2020_to_present =df_2020_to_present.withColumn("LON",col("LON").cast(DoubleType()))
+columns_to_change = ['Date Rptd', 'DATE OCC', 'Vict Age', 'LAT', 'LON']
+
+for field in new_schema.fields:
+    if field.name in columns_to_change:
+        if field.name != 'Date Rptd' and field.name != 'DATE OCC':
+            print("Changing the data type of column", field.name)
+            df = df.withColumn(field.name,df[field.name].cast(field.dataType))
+        else:
+            print("Changing the data type of column", field.name)
+            date_format = 'MM/dd/yyyy hh:mm:ss a'
+            #modified_df = modified_df.withColumn(field.name, to_date(unix_timestamp(df_2010_to_2019[field.name]), date_format).cast(field.dataType))
+            df = df.withColumn(field.name,to_date(field.name, date_format))
+
+
 #This is the dataframe that combines the other two dataframes
-df = df_2010_to_2019.union(df_2020_to_present)
+#df = modified_2010_to_2019.unionByName(modified_2020_to_present)
 
 print("Number of rows of original merged file is: ", df.count())
 
 #Now that we have the merged dataframe ready we will do some operations to our data
 #Firstly, we check if there are data before 2010
-df.filter(df['DATE OCC'] < '2010-01-01') 
+df.filter(df['DATE OCC'] < '2010/01/01') 
 print("Number of rows after filtering out crimes before 2010 is : ", df.count())
 
 #Then we check for duplicates
@@ -115,6 +123,12 @@ df = df.withColumn('Vict Descent', mapping_expr[df['Vict Descent']])
 df.select('Vict Descent').show(10)
 print('-'*100)
 
+print("After the changes the new schema is:")
+schemaString = df._jdf.schema().treeString()
+print(schemaString)
+print("-"*100)
+
+
 #Combine both files into one big file
 #First check if merged file already exists
 # -> if yes, do not create it again, for faster development
@@ -143,4 +157,11 @@ else:
       .option('delimiter', '|')
       .load(hdfs_file_dir + "project_data/Crime_Data_from_2010_to_Present.csv"))
 
-df.show(3 ,truncate = True)
+
+df.show(5, truncate=False)
+schemaString = df._jdf.schema().treeString()
+print(schemaString)
+print("-"*100)
+
+
+spark.stop()
